9333a4df222e23b5f8f476dff9f40c69cff7ae3b
RaduStefan30 <radustefan3010@gmail.com>
2025-08-09 19:23:26 +0300

    Replace email address

--- utils/testData.ts
+++ utils/testData.ts
@@ -1,6 +1,6 @@
 export const AUTH_DATA = {
     FIRST_NAME: 'Radu',
     LAST_NAME: 'Automation',
-    EMAIL_ADDRESS: 'radu.stefan.work@outlook.com',
+    EMAIL_ADDRESS: 'radu.stefan.test@outlook.com',
     PASSWORD: 'SecureP@ssw0rd'
 }
